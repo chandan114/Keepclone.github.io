c801b6c830c623a4fccd6687d721860132606e46
chandan maurya <chandan114>
2020-07-17 20:09:23 +0530

    Updates

---
+++ precache-manifest.7c6d324702f01fc28fecc2fa368c57c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175af4f0cf051f6f0e1618a9b16b1a58",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "eaae976f1d136ac19234",
+    "url": "/React-Projects/static/css/main.4649b3c5.chunk.css"
+  },
+  {
+    "revision": "0898f02e45620e84109f",
+    "url": "/React-Projects/static/js/2.9ba32006.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Projects/static/js/2.9ba32006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaae976f1d136ac19234",
+    "url": "/React-Projects/static/js/main.0d90b927.chunk.js"
+  },
+  {
+    "revision": "086d2758aa5606552223",
+    "url": "/React-Projects/static/js/runtime-main.a6c64fbf.js"
+  }
+]);
